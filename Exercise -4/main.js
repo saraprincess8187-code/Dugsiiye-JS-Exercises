d369dbe59fb2344686ca8f78712b2678a9843ecb
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-18 19:53:21 +0300

    Add Exercise -4

---
+++ Exercise -4/main.js
@@ -0,0 +1,15 @@
+// FUNCTION DECLARATION
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3));
+console.log(add(5, 7));
+console.log(add(10, 15));
+
+// FUNCTION EXPRESSION
+const sum = function (a, b){
+    return a + b;
+}
+console.log(sum(5, 3));
+console.log(sum(5, 9));
+console.log(sum(10, 5));
