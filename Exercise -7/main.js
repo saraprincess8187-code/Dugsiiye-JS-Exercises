5e65cfc7e5143dedc2d754b38be3f5a8da2eae6a
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-18 20:44:47 +0300

    Exercise 7

---
+++ Exercise -7/main.js
@@ -0,0 +1,15 @@
+let colors = ["red", "green", "blue"];
+
+colors.push("yellow");
+console.log(colors);
+
+colors.pop();
+console.log(colors);
+
+colors.unshift("purple");
+console.log(colors);
+
+colors.shift();
+console.log(colors);
+
+console.log(colors.length);
