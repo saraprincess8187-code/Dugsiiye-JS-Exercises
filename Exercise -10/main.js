efc2d8b490848ce20e9c677a57bc21f839874a9c
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-19 21:03:28 +0300

    10

---
+++ Exercise -10/main.js
@@ -0,0 +1,14 @@
+let library = [
+    {
+        title: "The Great Gatsby",
+        author: "John Doe",
+        year: 2020
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960
+    }
+];
+console.log(library[0].title);
+console.log(library[1].author);
