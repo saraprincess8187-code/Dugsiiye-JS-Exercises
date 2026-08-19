c8333ec9e06b363cbc72a865078b2cea3e2f813b
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-19 20:47:24 +0300

    Exercise 9

---
+++ Exercise -9/main.js
@@ -0,0 +1,12 @@
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    start: function() {
+        console.log("The car has started.");
+    }
+};
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+car.start();
