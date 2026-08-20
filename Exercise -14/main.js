e2e76e14378b6ea1bd26706b66c0886c7f6219d5
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-20 21:57:31 +0300

    14

---
+++ Exercise -14/main.js
@@ -0,0 +1,13 @@
+// let x = 10;
+// do {
+//     console.log("Iteration number: " + x);
+//     x--;
+// } while (x > 5);
+
+
+let number;
+do {
+    number = prompt("Enter a number greater than 10:");
+} while (number < 10);
+
+console.log("GOOD JOB! You entered.");
