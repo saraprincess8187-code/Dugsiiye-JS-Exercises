79acaec9d73875a01de01af5189765ae2c2fa9b7
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-21 21:47:27 +0300

    17

---
+++ Exercise -17/main.js
@@ -0,0 +1,10 @@
+let temperature = 30;
+if (temperature < 0) {
+    console.log("Very Cold");
+}else if (temperature >= 0 && temperature < 15) {
+    console.log("Cold");
+}else if (temperature <=15 && temperature < 25) {
+    console.log("Warm");
+}else {
+    console.log("Hot");
+}
