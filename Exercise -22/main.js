cb6932d2225fd815871df3d1e1b0b392ab9ef462
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-22 17:43:22 +0300

    22

---
+++ Exercise -22/main.js
@@ -0,0 +1,16 @@
+// const name = "Sacdia";
+// const city = "Mogadishu";
+
+// const message = `Hello ${name} Welcome to ${city}`
+// console.log(message)
+
+
+// const fruits = ["Banana", "Apple", "cherry"];
+
+// const [banana, apple, cherry] = fruits;
+
+// console.log(banana)
+
+const colors = ["red", "green", "blue"];
+const [first, second, third] = colors;
+console.log(first,third)
