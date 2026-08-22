32b45fa81d86eacc5dcf5fa545b4f49a94966d6c
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-22 17:25:38 +0300

    21

---
+++ Exercise -21/main.js
@@ -0,0 +1,14 @@
+//     //---------Filter()---------//
+// const numbers = [1,2,3,4,5,6];
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
+// console.log(evenNumbers);
+
+// // Filter() Method on Grades
+// const scores = [40,50,60,70,80,90];
+// const pass = scores.filter ((score) => score >= 60);
+// console.log(pass)
+
+// Reduce() method
+const numbers = [1,2,3,4,5,];
+const product = numbers.reduce((acc, num) => acc * num, 1);
+console.log(product)
