ed08ad66903603a96566634dd679af92e7e0a535
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-25 17:19:04 +0300

    31

---
+++ Exercise -31/main.js
@@ -0,0 +1,15 @@
+async function fetchUsers() {
+    try{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+
+        if(!response.ok) {
+            throw new Error(`HTTP Error! Status: ${response.status}`)
+        }
+        const users = await response.json();
+        console.log("Users:", users);
+    } catch(error) {
+        console.log("Erroe fetching users:", error);
+    };
+    
+}
+fetchUsers();
