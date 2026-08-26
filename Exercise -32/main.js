74001d2da8b0fea357642d2cc24bfca8e69d284c
saraprincess8187 <saraprincess8187@gmail.com>
2026-08-26 18:44:06 +0300

    32

---
+++ Exercise -32/main.js
@@ -0,0 +1,12 @@
+const name = document.querySelector('.name');
+console.log(name);
+
+const uni = document.querySelector('#university');
+console.log(uni);
+
+// querySelectorAll - selects ALL matches
+const allItems = document.querySelectorAll('li');
+console.log(allItems);
+
+const allParagraphs = document.querySelectorAll('p');
+console.log(allParagraphs);
